8b4c58320c570a1596ddd727b9a429f5bc55a999
Rob <rob.nard@gmail.com>
2016-11-21 19:06:40 -0500

    Skills Section [Mobile]
    - Created bar graphs (colour/spacing not final)
    - Added build in animation to skills graph (JS could be cleaned up and
    reduced)
    - Need to rework Hobbies section

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,7 +7,8 @@ $(function() {
 
     // Scroll function to animate the meters in
     // Function from Travis Neilson's YouTube channel. http://travisneilson.com/
-    var meter_class = ["drums", "hockey", "games", "tech"];
+    var meter_class = ["drums", "hockey", "games", "tech"],
+    skills_class = ["html", "css", "js", "liquid", "github"];
 
     $(window).scroll(function() {
         var vp_scroll = $(this).scrollTop();
@@ -15,6 +16,16 @@ $(function() {
             $(".meter-fill").each(function (i) {
                 setTimeout(function() {
                     $(".meter-fill").eq(i).addClass(meter_class[i]);
+                }, 150 * (i+1));
+            });
+        }
+    });
+    $(window).scroll(function() {
+        var vp_scroll = $(this).scrollTop();
+        if(vp_scroll > $(".skills-list-item").offset().top - ($(window).height() / 1.1)) {
+            $(".skills-list-item").each(function (i) {
+                setTimeout(function() {
+                    $(".skills-list-item").eq(i).addClass(skills_class[i]);
                 }, 150 * (i+1));
             });
         }
